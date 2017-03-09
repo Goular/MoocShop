82fd4d94c52a15b3aaeb753cdba777119f373087
Goular <zhaojt_cs@126.com>
2017-03-09 14:15:07 +0800

    提交代码的显示与控制器的创建

---
+++ controllers/OrderController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\controllers;
+
+class OrderController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    public function actionCheck()
+    {
+        return $this->renderPartial('check');
+    }
+
+}
