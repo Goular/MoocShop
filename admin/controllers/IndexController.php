5c38a1309edd0635011ac2dd83084231917e274f
Goular <zhaojt_cs@126.com>
2017-03-10 10:49:05 +0800

    删除多与文件，并添加后台首页

--- admin/controllers/IndexController.php
+++ admin/controllers/IndexController.php
@@ -4,19 +4,14 @@ namespace app\admin\controllers;
 
 use yii\web\Controller;
 
-/**
- * Default controller for the `mobile` module
- */
 class IndexController extends Controller
 {
     public $defaultAction = 'index';
-    /**
-     * Renders the index view for the module
-     * @return string
-     */
+    public $layout = "admin_main";
+
     public function actionIndex()
     {
-        $this->layout = 'layout_parent_none';
-        return $this->render('index');
+        //$this->layout = 'layout_parent_none';
+        return $this->renderPartial('index');
     }
 }
