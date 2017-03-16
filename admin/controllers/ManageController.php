9bb9a15a9581b109337997841f47e00374759eec
Goular <zhaojt_cs@126.com>
2017-03-16 14:48:38 +0800

    提交修改的页面

--- admin/controllers/ManageController.php
+++ admin/controllers/ManageController.php
@@ -21,7 +21,8 @@ class ManageController extends Controller
     //设置默认不需要模板
     public $layout = "admin_main";
 
-    public function actionMailchangepass(){
+    public function actionMailchangepass()
+    {
         $this->layout = false;
         //获取时间戳
         $time = \Yii::$app->request->get("timestamp");
@@ -29,42 +30,46 @@ class ManageController extends Controller
         $adminuser = \Yii::$app->request->get("adminuser");
         $token = \Yii::$app->request->get("token");
         $model = new Admin();
-        $myToken = $model->createToken($adminuser,$time);
-        if($token != $myToken){
+        $myToken = $model->createToken($adminuser, $time);
+        if ($token != $myToken) {
             //判读token是否一致
             $this->redirect(['public/login']);
             \Yii::$app->end();
         }
-        if(time()-$time>300){
+        if (time() - $time > 300) {
             //若有效期超过5分钟
             $this->redirect(['public/login']);
             \Yii::$app->end();
         }
-        if(\Yii::$app->request->isPost){
+        if (\Yii::$app->request->isPost) {
             //若当前的请求是post请求，即点击修改密码按钮
             $post = \Yii::$app->request->post();
-            if($model->changePass($post)){
+            if ($model->changePass($post)) {
                 //若改变密码成功，同时展示出来
-                \Yii::$app->session->setFlash('info','密码修改成功');
+                \Yii::$app->session->setFlash('info', '密码修改成功');
             }
         }
         $model->adminuser = $adminuser;
-        return $this->render("mailchangepass",['model'=>$model]);
+        return $this->render("mailchangepass", ['model' => $model]);
     }
 
-    public function actionReg(){
+    public function actionReg()
+    {
         return $this->render("reg");
     }
 
-    public function actionManagers(){
+    public function actionManagers()
+    {
         return $this->render("managers");
     }
 
-    public function actionChangeemail(){
+    public function actionChangeemail()
+    {
         return $this->render("changeemail");
     }
 
-    public function actionChangepass(){
+    public function actionChangepass()
+    {
         return $this->render("changepass");
     }
 }
