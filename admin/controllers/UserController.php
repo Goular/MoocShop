d1a9ae01a65fb3f33da2f440309957335f46d1ee
Goular <zhaojt_cs@126.com>
2017-03-16 14:15:38 +0800

    提交修改的页面

--- admin/controllers/UserController.php
+++ admin/controllers/UserController.php
@@ -4,6 +4,8 @@ namespace app\admin\controllers;
 
 class UserController extends \yii\web\Controller
 {
+    public $layout = "admin_main";
+
     public function actionIndex()
     {
         return $this->render('index');
