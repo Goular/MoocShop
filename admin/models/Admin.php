1e8f5e7400766c87f1a1daf2ec6eeba93e05c3f3
Goular <zhaojt_cs@126.com>
2017-03-18 01:05:51 +0800

    提交用户IP

--- admin/models/Admin.php
+++ admin/models/Admin.php
@@ -112,7 +112,7 @@ class Admin extends \yii\db\ActiveRecord
                 "adminuser" => $this->adminuser,
                 "isLogin" => 1
             ];
-            $this->updateAll(['logintime' => time(), 'loginip' => ip2long(Yii::$app->request->userIP)], 'adminuser= :user', [':user' => $this->adminuser]);
+            $this->updateAll(['logintime' => time(), 'loginip' => ip2long(\Yii::$app->request->userIP)], 'adminuser= :user', [':user' => $this->adminuser]);
             return (bool)$session['admin']['isLogin'];
         }
         return false;
