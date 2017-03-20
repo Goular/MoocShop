a8ac5d7bbb1f049a1c88381d4835c39d23be6c8c
Goular <zhaojt_cs@126.com>
2017-03-20 18:00:41 +0800

    提交分类控制器文档

--- models/Category.php
+++ models/Category.php
@@ -3,6 +3,7 @@
 namespace app\models;
 
 use Yii;
+use yii\helpers\ArrayHelper;
 
 /**
  * This is the model class for table "{{%category}}".
@@ -19,15 +20,6 @@ class Category extends \yii\db\ActiveRecord
         return '{{%category}}';
     }
 
-    public function rules()
-    {
-        return [
-            ['parentid', 'required', 'message' => '上级分类不能为空'],
-            ['title', 'required', 'message' => '标题名称不能为空'],
-            ['createtime', 'safe']
-        ];
-    }
-
     public function attributeLabels()
     {
         return [
@@ -38,20 +30,49 @@ class Category extends \yii\db\ActiveRecord
         ];
     }
 
+    public function rules()
+    {
+        return [
+            ['parentid', 'required', 'message' => '上级分类不能为空'],
+            ['title', 'required', 'message' => '标题名称不能为空'],
+            ['createtime', 'safe']
+        ];
+    }
+
     //添加分类的内容
-    public function add($data){
+    public function add($data)
+    {
         //在添加分类的时候我们都需要添加创建时间
         $data['Category']['createtime'] = time();
-        if($this->load($data) && $this->save()){
+        if ($this->load($data) && $this->save()) {
             return true;
         }
         return false;
     }
 
-    //获取选择分类的选项
-    public function getOptions(){
-        $options = ['添加顶级分类'];
-        return $options;
+    //获取所有的数据
+    public function getData()
+    {
+        $cates = self::find()->all();
+        //利用帮助类将对象列表转为数组
+        $cates = ArrayHelper::toArray($cates);
+        return $cates;
+    }
+
+    //获取分类的树状图
+
+    //其实传智的ThinkPHP中rbac更好
+    //即在创建时，添加好分层分级
+    //101-105,101-105-110,这样使用orderBy来进行获去，就不用每次都排序了，毕竟递归的内容的消耗重复很厉害
+    public function getTree($cates, $pid = 0)
+    {
+        $tree = [];
+        foreach ($cates as $cate) {
+            //遍历统一等级，同时做一个
+            if ($cate['parentid'] == $pid) {
+                $tree[] = $cate;
+            }
+        }
     }
 
 }
