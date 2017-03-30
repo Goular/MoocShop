daa3fcd56da1a271139faa38eedf171ce6cc8b8d
Goular <zhaojt_cs@126.com>
2017-03-30 11:22:01 +0800

    提交添加购物车的内容

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -2,12 +2,92 @@
 
 namespace app\controllers;
 
+use app\models\Cart;
+use app\models\Product;
+use app\models\User;
+use Yii;
+
 class CartController extends \yii\web\Controller
 {
+    public $layout = 'layout_parent_none';
+
+    /**
+     * 首页
+     * @return string
+     */
     public function actionIndex()
     {
-        $this->layout = 'layout_parent_none';
-        return $this->render('index');
+        if (\Yii::$app->session['isLogin'] != 1) {
+            return $this->redirect(['member/auth']);
+        }
+        $userid = User::find()->where('username = :name or useremail = :email', [':name' => Yii::$app->session['loginname'], ':email' => Yii::$app->session['loginname']])->one()->userid;
+        $cart = Cart::find()->where('userid = :uid', [':uid' => $userid])->asArray()->all();
+        $data = [];
+        foreach ($cart as $k => $pro) {
+            $product = Product::find()->where('productid = :pid', [':pid' => $pro['productid']])->one();
+            $data[$k]['cover'] = $product->cover;
+            $data[$k]['title'] = $product->title;
+            $data[$k]['productnum'] = $pro['productnum'];
+            $data[$k]['price'] = $pro['price'];
+            $data[$k]['productid'] = $pro['productid'];
+            $data[$k]['cartid'] = $pro['cartid'];
+        }
+        return $this->render("index", ['data' => $data]);
     }
 
+    /**
+     * 添加商品到购物车
+     */
+    public function actionAdd()
+    {
+        //由于添加到购物车有两个渠道
+        //1.从普通的推荐商品和热卖商品中添加(Get请求)
+        //2.从商品详情页中添加(Post请求)
+        if (\Yii::$app->session['isLogin'] != 1) {
+            return $this->redirect(['member/auth']);
+        }
+        $userid = User::find()->where('username = :name or useremail = :email', [':name' => Yii::$app->session['loginname'], ':email' => Yii::$app->session['loginname']])->one()->userid;
+        if (\Yii::$app->request->isPost) {
+            $post = \Yii::$app->request->post();
+            $num = \Yii::$app->request->post()['productnum'];
+            $data['Cart'] = $post;
+            $data['Cart']['userid'] = $userid;
+        }
+        if (\Yii::$app->request->isGet) {
+            $productid = \Yii::$app->request->get("productid");
+            $model = Product::find()->where('productid = :pid', [':pid' => $productid])->one();
+            $price = $model->issale ? $model->saleprice : $model->price;
+            $num = 1;
+            $data['Cart'] = ['productid' => $productid, 'productnum' => $num, 'price' => $price, 'userid' => $userid];
+        }
+        if (!$model = Cart::find()->where('productid = :pid and userid = :uid', [':pid' => $data['Cart']['productid'], ':uid' => $data['Cart']['userid']])->one()) {
+            $model = new Cart;
+        } else {
+            $data['Cart']['productnum'] = $model->productnum + $num;
+        }
+        $data['Cart']['createtime'] = time();
+        $model->load($data);
+        $model->save();
+        return $this->redirect(['cart/index']);
+    }
+
+    /**
+     * 修改购物车商品
+     */
+    public function actionMod()
+    {
+        $cartid = \Yii::$app->request->get('cartid');
+        $productnum = \Yii::$app->request->get("productnum");
+        Cart::updateAll(['productnum' => $productnum], 'cartid = :cid', [':cid' => $cartid]);
+    }
+
+    /**
+     * 删除购物车商品
+     */
+    public function actionDel()
+    {
+        $cartid = \Yii::$app->request->get('cartid');
+        Cart::deleteAll('cartid = :cid', [':cid' => $cartid]);
+        return $this->redirect(['cart/index']);
+    }
 }
