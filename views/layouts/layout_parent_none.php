f33842c2dd6421e0e33c75905333e7f280f7c5d0
Goular <zhaojt_cs@126.com>
2017-03-30 17:05:52 +0800

    提交修改和删除购物车的逻辑和内容

--- views/layouts/layout_parent_none.php
+++ views/layouts/layout_parent_none.php
@@ -592,6 +592,50 @@ use yii\helpers\Url;
 <script src="/assets/js/jquery.customSelect.min.js"></script>
 <script src="/assets/js/wow.min.js"></script>
 <script src="/assets/js/scripts.js"></script>
-
+<script>
+    $("#createlink").click(function () {
+        $('.billing-address').slideDown();
+    });
+    $(".minus").click(function () {
+        var cartid = $("input[name=productnum]").attr('id');
+        var num = parseInt($("input[name=productnum]").val()) - 1;
+        if (parseInt($("input[name=productnum]").val()) <= 1) {
+            var num = 1;
+        }
+        var total = parseFloat($(".value.pull-right span").html());
+        var price = parseFloat($(".price span").html());
+        changeNum(cartid, num);
+        var p = total - price;
+        if (p < 0) {
+            var p = "0";
+        }
+        $(".value.pull-right span").html(p + "");
+        $(".value.pull-right.ordertotal span").html(p + "");
+    });
+    $(".plus").click(function () {
+        var cartid = $("input[name=productnum]").attr('id');
+        var num = parseInt($("input[name=productnum]").val()) + 1;
+        var total = parseFloat($(".value.pull-right span").html());
+        var price = parseFloat($(".price span").html());
+        changeNum(cartid, num);
+        var p = total + price;
+        $(".value.pull-right span").html(p + "");
+        $(".value.pull-right.ordertotal span").html(p + "");
+    });
+    function changeNum(cartid, num) {
+        $.get('<?php echo Url::to(['cart/mod']);?>', {'productnum': num, 'cartid': cartid}, function (data) {
+            location.reload();
+        });
+    }
+    var total = parseFloat($("#total span").html());
+    $(".le-radio.express").click(function () {
+        var ototal = parseFloat($(this).attr('data')) + total;
+        $("#ototal span").html(ototal);
+    });
+    $("input.address").click(function () {
+        var addressid = $(this).val();
+        $("input[name=addressid]").val(addressid);
+    });
+</script>
 </body>
 </html>
